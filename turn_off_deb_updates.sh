c8e39f67d7573fd3ee9fecac89809dd29547e80c
Alpatiev Nikita <118742566+alpatiev@users.noreply.github.com>
2024-05-07 19:05:51 +0400

    Create turn_off_deb_updates.sh

---
+++ turn_off_deb_updates.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Disable all updates in Debian/Ubuntu.
+# TODO: check another services.
+
+# 1. Disable unattended upgrades
+sudo systemctl stop unattended-upgrades
+sudo systemctl disable unattended-upgrades
+
+# 2. Disable PackageKit
+sudo systemctl stop packagekit
+sudo systemctl disable packagekit
+
+# 3. Disable Snapd
+sudo systemctl stop snapd
+sudo systemctl disable snapd
+
+# 4. Disable apt-daily services
+sudo systemctl stop apt-daily.timer
+sudo systemctl stop apt-daily-upgrade.timer
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.timer
