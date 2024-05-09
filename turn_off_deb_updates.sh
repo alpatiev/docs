5f2c23a6b0d5842453608dd03f55e20ff6c61c1b
Alpatiev Nikita <118742566+alpatiev@users.noreply.github.com>
2024-05-09 17:56:21 +0400

    Update turn_off_deb_updates.sh

--- turn_off_deb_updates.sh
+++ turn_off_deb_updates.sh
@@ -20,3 +20,6 @@ sudo systemctl stop apt-daily.timer
 sudo systemctl stop apt-daily-upgrade.timer
 sudo systemctl disable apt-daily.timer
 sudo systemctl disable apt-daily-upgrade.timer
+
+# 5. Purge DigitalOcean metrics agent
+sudo apt-get purge do-agent
