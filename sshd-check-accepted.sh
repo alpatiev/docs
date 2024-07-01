cdfe4848ee0bc2949edc75ecf02645ec7fa6a18e
Alpatiev Nikita <118742566+alpatiev@users.noreply.github.com>
2024-07-01 21:00:16 +0400

    Update sshd-check-accepted.sh

--- sshd-check-accepted.sh
+++ sshd-check-accepted.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-SSH_LOG_PATH="/var/log/auth.log"
+SSH_LOG_DIR="/var/log"
+SSH_LOG_FILES=("auth.log" "auth.log.*" "auth.log.*.gz")
+TEMP_FILE=".ssh-temp-batch"
 STAMP="[  *  ]"
 
 show_selection() {
@@ -12,7 +14,24 @@ show_selection() {
   done
 }
 
-entries=$(grep 'Accepted' $SSH_LOG_PATH | awk '{print $1, $2, $3, $11, $9}')
+extract_entries() {
+  > $TEMP_FILE
+  for file in "${SSH_LOG_FILES[@]}"; do
+    for log in $SSH_LOG_DIR/$file; do
+      if [[ -f $log ]]; then
+        if [[ $log == *.gz ]]; then
+          zgrep 'Accepted' "$log" >> $TEMP_FILE
+        else
+          grep 'Accepted' "$log" >> $TEMP_FILE
+        fi
+      fi
+    done
+  done
+}
+
+extract_entries
+
+entries=$(grep 'Accepted' $TEMP_FILE | awk '{print $1, $2, $3, $11, $9}')
 ips=($(echo "$entries" | awk '{print $5}' | sort | uniq))
 
 show_selection
@@ -21,11 +40,12 @@ while true; do
   read -p "$STAMP Select [1-${#ips[@]}] or [q]: " selection
 
   if [[ $selection == "q" ]]; then
+    rm $TEMP_FILE
     exit 0
   elif [[ $selection -ge 1 && $selection -le ${#ips[@]} ]]; then
     selected_ip=${ips[$((selection-1))]}
     echo "$STAMP Details for entries with $selected_ip:"
-    echo "$entries" | grep "$selected_ip"
+    grep "$selected_ip" $TEMP_FILE
     read -p "$STAMP Check again [y/n]: " check_again
     if [[ $check_again == "n" ]]; then
       break
@@ -36,3 +56,5 @@ while true; do
     echo "$STAMP Select from 1 to ${#ips[@]} or 'q' to quit."
   fi
 done
+
+rm $TEMP_FILE
